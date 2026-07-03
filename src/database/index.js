7a7384f5bed87e2164c469fbb5140173350800d9
Glenon Mateus <glenonmateus+github@gmail.com>
2026-07-02 22:17:16 -0300

    test: database connection test

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,14 @@
 import Sequelize from "sequelize";
 import databaseConfig from "../config/database.js";
 
-const connection = new Sequelize(`${databaseConfig}.${process.env.NODE_ENV}`);
+const connection = new Sequelize(databaseConfig[process.env.NODE_ENV]);
+
+try {
+  await connection.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database: ", error);
+}
 
 const models = [];
 
